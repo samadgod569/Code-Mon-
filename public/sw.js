d2a635dd337b7e2b7683476b733d7fa1ffaa0bb7
samadgod569 <hellohikaisiho124@gmail.com>
2025-12-28 12:45:28 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,34 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
+const CACHE_NAME = "pwa-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/test.html",
+  "/manifest.json",
+  "/@me.html"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
 });
 
-self.addEventListener('fetch', () => {});
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(
+      cached => cached || fetch(event.request)
+    )
+  );
+});
