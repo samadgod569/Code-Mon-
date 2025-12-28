496f78068b9a7c8d5b5ca845982177bbb327a111
samadgod569 <hellohikaisiho124@gmail.com>
2025-12-28 11:45:46 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,5 @@
-const CACHE_NAME = 'app-v1';
-const urlsToCache = [
-  
-  './test.html',
-  './@me.html',
-  './C69P2W_newllo.png'
-];
-
-// Install
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting();
 });
 
-// Activate
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
-  ));
-});
-
-// Fetch
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
-  );
-});
+self.addEventListener('fetch', () => {});
